edbd960d8e5cfbb9343710937dbfbbcc32208a2f
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-07-24 00:34:50 +0000

    added authorization for lessons create page and refactored code

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,12 +1,13 @@
 class Instructor::LessonsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_selection
+
+
   def new
-    @section = Section.find(params[:section_id])
     @lesson = Lesson.new
   end
 
   def create
-    @section = Section.find(params[:section_id])
     @lesson = @section.lessons.create(lesson_params)
     redirect_to instructor_course_path(@section.course)
   end
@@ -15,6 +16,17 @@ class Instructor::LessonsController < ApplicationController
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
+  end
+
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
+
+  def require_authorized_for_current_selection
+    if current_section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
   end
 
 end
