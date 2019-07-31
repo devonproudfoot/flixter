30f1b3c84f4995c9535f85bbe1b22be6339225e6
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-07-31 18:24:26 +0000

    Updated carrierwave to save to aws, created uploader for videos

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,10 +1,11 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+  # storage :fog
   storage :aws
 
   # Override the directory where uploaded files will be stored.
@@ -27,7 +28,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-  process resize_to_fill: [800, 350]
 
   # Create different versions of your uploaded files:
   # version :thumb do
