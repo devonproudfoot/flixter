a335b74fb76fd7fe5b22ae84b26c69f22e9ca7d9
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-08-05 01:00:40 +0000

    Restricted lesson pages to users that are enrolled in the related course.  Need to refactor the enrolled_in? function in the user model

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_lesson_view, only: [:show]
 
   def show
   end
@@ -8,6 +10,12 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrollment_for_lesson_view
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must register for the course to view the lessons!'
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   has_many :courses
   has_many :enrollments
-  has_many :enrolled_courses, through: :enrollments, source: :course
+  # has_many :enrolled_courses, through: :enrollments, source: :course
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+# Need to refactor this using the through statement! But it works for now!
   def enrolled_in?(course)
+    enrolled_courses = enrollments.collect(&:course)
     return enrolled_courses.include?(course)
   end
 end
