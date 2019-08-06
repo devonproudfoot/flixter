40122724e7e13d9ea1df13ddb2f719b242c4018b
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-08-06 21:17:52 +0000

    added ranked_model gem and sorted the lessons by rank

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,9 @@
 class Lesson < ApplicationRecord
   belongs_to :section
   mount_uploader :video, VideoUploader
+
+  include RankedModel
+
+  belongs_to :section
+  ranks :row_order, :with_same => :section_id
 end
