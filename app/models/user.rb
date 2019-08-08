01765e413ef5ea5df0f26466a98e1a3550d2c277
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-08-08 02:00:05 +0000

    Made javascript into a single function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :courses
   has_many :enrollments
-  # has_many :enrolled_courses, through: :enrollments, source: :course
+  has_many :enrolled_courses, :through => :enrollments, :source => :course
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -10,7 +10,9 @@ class User < ApplicationRecord
 
 # Need to refactor this using the through statement! But it works for now!
   def enrolled_in?(course)
+    logger.debug('look here')
     enrolled_courses = enrollments.collect(&:course)
     return enrolled_courses.include?(course)
+    # return enrolled_courses.include?(course)
   end
 end
